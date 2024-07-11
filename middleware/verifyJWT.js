b718b1c41bdcf98e9e4d5f4a03dd06317bad9879
Daniel <156960927+U22099@users.noreply.github.com>
2024-07-11 11:28:10 +0100

    Add files via upload

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const verifyJWT = (req, res, next) => {
-    const token = req.cookies.accessToken
+    const token = req.headers.Authorization.accessToken
     if(!token){
         return res.sendStatus(401)
     };
