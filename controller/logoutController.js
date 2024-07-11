1e0c990d153c02bde469a5edd7d93aa03ac71825
Daniel <156960927+U22099@users.noreply.github.com>
2024-07-11 19:10:34 +0100

    Update logoutController.js

--- controller/logoutController.js
+++ controller/logoutController.js
@@ -4,8 +4,8 @@ const logOut = async (req, res) => {
     if(!accessToken) return res.sendStatus(401);
     const user = await User.findOne({ accessToken: accessToken });
 
-    user.refreshToken = "";
-    user.accessToken = "";
+    user?.refreshToken = "";
+    user?.accessToken = "";
     await user.save();
     //res.cookie('refreshToken', '', {
         //expires: new Date('1970-01-01'),
