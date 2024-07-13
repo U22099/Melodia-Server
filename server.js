4b3960fb70efd56c4c0d36c7fc91e0f10632455e
Daniel <156960927+U22099@users.noreply.github.com>
2024-07-13 02:00:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -45,3 +45,5 @@ mongoose.connection.once('open', () => {
     app.listen(PORT, ()=> console.log(`Server is running on port: ${PORT}`));
     console.log('Connected to Database');
 });
+
+module.exports = app;
