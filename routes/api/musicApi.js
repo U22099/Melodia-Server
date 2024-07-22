e903a233a7a6db18ca79f03114ca31285697bdf4
Daniel <156960927+U22099@users.noreply.github.com>
2024-07-23 00:56:16 +0100

    Update musicApi.js

--- routes/api/musicApi.js
+++ routes/api/musicApi.js
@@ -4,5 +4,6 @@ const musicApiController = require('../../controller/api/musicApiController');
 
 router.get('/', musicApiController.getMusic);
 router.post('/', musicApiController.addMusic);
+router.post('/data', musicApiController.getMusicById);
 
 module.exports = router;
