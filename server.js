11d1fa773d67e0043c8dc4c3c9e72686cc2c59f3
Daniel <156960927+U22099@users.noreply.github.com>
2024-07-25 02:39:29 +0100

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 })
 app.use(express.urlencoded({ extended: false}));
 
-app.use(express.json({ limit: "4mb" }));
+app.use(express.json({ limit: "100mb" }));
 app.use(cookieParser());
 app.use('/', require('./routes/home'));
 app.use('/register', require('./routes/register'));
