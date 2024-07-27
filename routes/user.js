98ba721e2a92c986128e9f70c315f74b3899c793
Daniel <156960927+U22099@users.noreply.github.com>
2024-07-27 08:11:10 +0100

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const userController = require('../controller/userController');
 
 router.get('/', userController.getData);
+router.get('/admin', userController.getAdminData);
 router.put('/', userController.updateData);
 router.delete('/', userController.deleteUser);
 
