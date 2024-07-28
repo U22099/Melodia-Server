8eb4de04e703cbcd8653d3c3c8c8f15419edf781
Daniel <156960927+U22099@users.noreply.github.com>
2024-07-28 13:35:50 +0100

    Update User.js

--- model/User.js
+++ model/User.js
@@ -18,6 +18,10 @@ const userSchema = new Schema({
         type: String,
         default: ''
     },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    }
     refreshToken: String,
     accessToken: String
 });
