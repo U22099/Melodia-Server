358ea7a83c9287173c9370bd73863a455818b224
Daniel <156960927+U22099@users.noreply.github.com>
2024-08-01 14:20:45 +0000

    —-

--- config/DatabaseConn.js
+++ config/DatabaseConn.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try{
-        await mongoose.connect(process.env.DATABASE_URL, {});
+        await mongoose.connect(process.env.DATABASE_URL, {
+            allowDiskUse: true
+        });
     } catch(err){
         console.error(err)
     }
